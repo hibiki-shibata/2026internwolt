4967095f256cbfd5747cf80d0b4bf3e3a2e400ec
hibiki-shibata <hibiki.shibata@wolt.com>
2026-01-11 03:39:13 +0900

    Add: calculateDistanceTest

---
+++ app/src/test/kotlin/org/dopc/service/deliveryDistance/calculateDistanceTest.kt
@@ -0,0 +1,39 @@
+package org.dopc.service.deliveryDistance
+
+import kotlin.test.*
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlin.math.abs
+import kotlinx.coroutines.*
+import org.dopc.model.Coordinates
+
+class CalculateDistanceTest {
+
+    @Test
+    fun `should return 0 for identical coordinates`() = runBlocking {
+        val venueCoordinates = Coordinates(lon=0.0, lat=0.0)
+        val userCoordinates = Coordinates(lon=0.0, lat=0.0)
+        
+        val result = calculateDistance(venueCoordinates, userCoordinates)
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun `should calculate large distance correctly`() = runBlocking {
+        val venueCoordinates = Coordinates(lon=-1.602051, lat = 47.155047)  // Nantes Airport coordinates 
+        val userCoordinates = Coordinates(lon=139.7428526, lat=35.6585848)   // Tokyo Tower coordinates 
+        
+        val result = calculateDistance(venueCoordinates, userCoordinates)
+        assertTrue(abs(result - 10033000.0) < 1000.0) // approximately 10033 km with a tolerance of 1 km
+    }
+
+    @Test
+    fun `should calculate distance correctly with negative lat and positive lat coordinates`() = runBlocking {
+        val venueCoordinates = Coordinates(lon=139.74546295402249, lat=35.658622906567)  // Tokyo tower
+        val userCoordinates = Coordinates(lon=151.176456945061, lat=-33.9405639972) // Sydoney Airport
+        
+        val result = calculateDistance(venueCoordinates, userCoordinates)
+        assertTrue(abs(result - 7829000.0) < 3000.0) // approximately 7829 km with a tolerance of 3 km
+    }
+
+}
