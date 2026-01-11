6f57a65e1438b99037a1c31a7febf44aea56d637
hibiki-shibata <hibiki.shibata@wolt.com>
2026-01-11 19:58:35 +0900

    Add: routing

--- app/src/main/kotlin/org/dopc/routing.kt
+++ app/src/main/kotlin/org/dopc/routing.kt
@@ -1,13 +1,15 @@
+// Doc: Ktor routing: https://ktor.io/docs/server-routing.html#nested_routes
 package org.dopc
 
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.dopc.routing.apiV1
 
 fun Application.configureRouting() {
-    routing {
-        get("/") {
-            call.respondText("Hello World!")
+    routing{
+        route("/api/v1/") {
+            apiV1()
         }
     }
 }
---
+++ app/src/main/kotlin/org/dopc/routing/apiV1.kt
@@ -0,0 +1,21 @@
+package org.dopc.routing
+
+import io.ktor.server.routing.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import org.dopc.service.DopcService
+import org.dopc.dto.DopcReqDTO
+
+
+fun Route.apiV1() {
+    get("/delivery-order-price") {    
+        val req = DopcReqDTO(
+            venue_slug = call.request.queryParameters["venue_slug"] ?: throw IllegalArgumentException("venue_slug is required"),
+            cart_value = call.request.queryParameters["cart_value"]?.toInt() ?: throw IllegalArgumentException("cart_value is required"),
+            user_lon = call.request.queryParameters["user_lon"]?.toDouble() ?: throw IllegalArgumentException("user_lon is required"),
+            user_lat = call.request.queryParameters["user_lat"]?.toDouble() ?: throw IllegalArgumentException("user_lat is required")
+        )
+        val res = DopcService().calculate(req)
+        call.respondText(res.toString())
+    }
+}
